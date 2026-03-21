00647d8161bd67f0bd2c0599489a6f1d0d38b25a
Rong-Rong11 <phungenzo1001@gmail.com>
2026-03-21 20:37:03 +0100

    enleve les import inutile

--- src/gui/dashboard/RosterDashboard.java
+++ src/gui/dashboard/RosterDashboard.java
@@ -2,7 +2,6 @@ package gui.dashboard;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.util.ArrayList;
 
--- src/gui/panel/mapPanel/effectifPanel/MapTeamSummaryPanel.java
+++ src/gui/panel/mapPanel/effectifPanel/MapTeamSummaryPanel.java
@@ -2,7 +2,6 @@ package gui.panel.mapPanel.effectifPanel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
