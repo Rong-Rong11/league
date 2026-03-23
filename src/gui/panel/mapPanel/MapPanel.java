e6d242888ffa8e9dfc0e1e68bc2ab4fd34fe8682
Rong-Rong11 <phungenzo1001@gmail.com>
2026-03-23 09:42:00 +0100

    suppression de class inutile

--- src/gui/panel/mapPanel/MapPanel.java
+++
@@ -1,6 +0,0 @@
-package gui.panel.mapPanel;
-
-import gui.panel.common.TeamMapPanel;
-
-public class MapPanel extends TeamMapPanel {
-}
--- src/gui/panel/openningPanel/OpeningMapPanel.java
+++
@@ -1,6 +0,0 @@
-package gui.panel.openningPanel;
-
-import gui.panel.common.TeamMapPanel;
-
-public class OpeningMapPanel extends TeamMapPanel {
-}
