1a2fb7c35fc4ded5a17ced17dab8553a45a9de6c
FatmaZahraBaarir <fatma-zahra.baarir@etu.cyu.fr>
2026-03-28 22:35:11 +0100

    commit

--- src/gui/app/App.java
+++ src/gui/app/App.java
@@ -1,5 +1,7 @@
 package gui.app;
 
+
+
 import gui.frame.MainGui;
 import process.SimulationInterface;
 import process.manager.SimulationManager;
@@ -9,5 +11,6 @@ public class App {
    public static void main(String[] args) {
       SimulationInterface simulationInterface = new SimulationManager();
       MainGui gui = new MainGui(simulationInterface);
+      
    }
 }
