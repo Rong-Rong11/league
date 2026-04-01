894910439ce6ebac4935c0feffe953200f76cec6
Fatizb <mimi.baarir@gmail.com>
2026-04-01 19:52:20 +0200

    co

--- src/gui/app/App.java
+++ src/gui/app/App.java
@@ -6,9 +6,9 @@ import process.orchestrator.SimulationManager;
 
 public class App {
 
-   public static void main(String[] args) {
-      SimulationInterface simulationInterface = new SimulationManager();
-      MainGui gui = new MainGui(simulationInterface);
+	public static void main(String[] args) {
+		SimulationInterface simulationInterface = new SimulationManager();
+		MainGui gui = new MainGui(simulationInterface);
 
-   }
+	}
 }
