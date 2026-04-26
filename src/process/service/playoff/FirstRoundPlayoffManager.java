0609fd2a4ff26585b42acfe774b4767b24482b67
Rong-Rong11 <phungenzo1001@gmail.com>
2026-04-26 14:14:41 +0200

    correction

--- src/process/service/playoff/FirstRoundPlayoffManager.java
+++ src/process/service/playoff/FirstRoundPlayoffManager.java
@@ -50,11 +50,11 @@ public class FirstRoundPlayoffManager extends PlayoffManager {
 		logger.debug("Advancing from first round to conference semifinals");
 
 		League league = getLeague();
-		league.setPlayoff(getPlayoffBuilder().buldSecondRoundPlayoffs());
+		league.setPlayoff(getPlayoffBuilder().buildSecondRoundPlayoffs());
 		league.getPlayoff().setCurrentRound(PlayoffRound.CONFERENCE_SEMIFINALS);
 
 		SemiCalendarBuilder semiCalendarBuilder = new SemiCalendarBuilder(league, roundEndDate);
-		league.getPlayoff().setNbaCalendar(semiCalendarBuilder.buildCalendar());
+		mergePlayoffCalendar(semiCalendarBuilder.buildCalendar());
 
 		logger.debug("Conference semifinals playoffs and calendar created");
 	}
